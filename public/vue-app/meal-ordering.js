8eebee7ccc8d72cd0aa614c947e13ff9b47228d0
Raphael Odini <raphael.odini@gmail.com>
2018-02-15 13:02:29 +0800

    finish feedback modal. post body to the backend

--- public/vue-app/meal-ordering.js
+++ public/vue-app/meal-ordering.js
@@ -20,6 +20,12 @@ Vue.component('my-order-list', {
       lastUpdated: undefined
     }
   },
+  created: function() {
+    bus.$on('refreshOrderList', this.getOrderList);
+  },
+  mounted: function() {
+    this.getOrderList();
+  },
   methods: {
     getOrderList: function() {
       this.loadingOrders = true;
@@ -39,9 +45,6 @@ Vue.component('my-order-list', {
     openFeedbackModal: function(order) {
       bus.$emit('showModal', order);
     }
-  },
-  mounted: function() {
-    this.getOrderList();
   }
 });
 
@@ -51,7 +54,8 @@ Vue.component('my-feedback-modal', {
   data: function() {
     return {
       displayModal: 'none',
-      currentOrder: {}
+      order: {},
+      feedbackPostObject: { feedbacks: [] }
     }
   },
   created: function() {
@@ -59,16 +63,73 @@ Vue.component('my-feedback-modal', {
   },
   methods: {
     openModal: function(order) {
-      console.log('openModal', order);
       this.displayModal = 'block';
-      this.currentOrder = order;
+      this.order = order;
+    },
+    rateAllMeals: function(rating) {
+      this.order.order_items.forEach((item, index) => {
+        this.$set(this.order.order_items[index], 'rating', rating);
+      });
+    },
+    rateMeal: function(index, rating) {
+      // this.order.order_items[index] = Object.assign({}, this.order.order_items[index], { rating: rating });
+      this.$set(this.order.order_items[index], 'rating', rating);
+    },
+    toggleMealComment: function(index) {
+      this.$set(this.order.order_items[index], 'showCommentInput', (this.order.order_items[index].showCommentInput ? !this.order.order_items[index].showCommentInput : true));
+      if (!this.order.order_items[index].showCommentInput) {
+        this.$set(this.order.order_items[index], 'comment', '');
+      }
+    },
+    rateOrder: function(rating) {
+      // this.order = Object.assign({}, this.order, { rating: rating });
+      this.$set(this.order, 'rating', rating);
+    },
+    toggleOrderComment: function() {
+      this.$set(this.order, 'showCommentInput', (this.order.showCommentInput ? !this.order.showCommentInput : true));
+      if (!this.order.showCommentInput) {
+        this.$set(this.order, 'comment', '');
+      }
     },
     saveFeedback: function() {
-      console.log('saveFeedback');
+      // add DeliveryOrder
+      this.feedbackPostObject.feedbacks.push({
+        ratable_id: this.order.id,
+        ratable_type: 'DeliveryOrder',
+        rating: this.order.rating,
+        comment: this.order.comment
+      });
+      // add OrderItems
+      this.order.order_items.forEach((item, index) => {
+        this.feedbackPostObject.feedbacks.push({
+          ratable_id: item.order_item_id,
+          ratable_type: 'OrderItem',
+          rating: item.rating,
+          comment: item.comment
+        });
+      });
+      // send feedbacks
+      fetch(`/orders/${this.order.order_id}/feedbacks`, {
+        method: 'POST',
+        body: JSON.stringify(this.feedbackPostObject),
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      })
+        .then(res => res.json())
+        .then((data) => {
+          this.closeModal(true);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     },
-    closeModal: function() {
-      // this.$emit('close');
+    closeModal: function(saved) {
+      this.feedbackPostObject = { feedbacks: [] };
       this.displayModal = 'none';
+      if (saved) {
+        bus.$emit('refreshOrderList');
+      }
     }
   }
 });
@@ -81,7 +142,7 @@ Vue.component('my-feedback-modal', {
 Vue.filter('formatDateTime', function(value) {
   if (value) {
     return value.toLocaleString(undefined, {
-      weekday: 'short',
+      weekday: 'long',
       day: '2-digit',
       month: '2-digit',
       year: 'numeric',
@@ -94,7 +155,7 @@ Vue.filter('formatDateTime', function(value) {
 Vue.filter('formatDate', function(value) {
   if (value) {
     return value.toLocaleDateString(undefined, {
-      weekday: 'short',
+      weekday: 'long',
       day: '2-digit',
       month: '2-digit',
       year: 'numeric'
